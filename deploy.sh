15cfda73b477ca4a84a876af68d13d25d9414b30
Pierre Jacquet <44034432+jacquetpi@users.noreply.github.com>
2022-10-28 11:39:11 +0200

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-ssh pijacque@spirals-tornado.lille.inria.fr "cd /usr/local/src/vmprobe/ && rm -r src/*"
-scp -r src/* pijacque@spirals-tornado.lille.inria.fr:/usr/local/src/vmprobe/src
-ssh pijacque@spirals-tornado.lille.inria.fr "cd /usr/local/src/vmprobe/ && ./compil.sh"
